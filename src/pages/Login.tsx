6f41331bd2e2e98f1398eeab8b3adf96ede7130a
edosheram <cheramrodriguez724@gmail.com>
2025-03-10 14:29:03 +0800

    added feature/loginSignup

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,31 +1,86 @@
-import { 
+import {
+  IonAvatar,
   IonButton,
   IonButtons,
-    IonContent, 
-    IonHeader, 
-    IonMenuButton, 
-    IonPage, 
-    IonTitle, 
-    IonToolbar, 
-    useIonRouter
-} from '@ionic/react';
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonInputPasswordToggle,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonMenuButton,
+  IonPage,
+  IonThumbnail,
+  IonTitle,
+  IonToolbar,
+  useIonRouter,
+} from "@ionic/react";
 
 const Login: React.FC = () => {
   const navigation = useIonRouter();
   const doLogin = () => {
-      navigation.push('/it35-lab/app','forward','replace');
-  }
+    navigation.push("/it35-lab/app", "forward", "replace");
+  };
   return (
+    //HEADER
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonTitle>Login</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent className='ion-padding'>
-          <IonButton onClick={() => doLogin()} expand="full">
-              Login
-          </IonButton>
+
+      <IonContent className="ion-padding">
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle></IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>
+            <IonList>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  height: "100%",
+                }}
+              >
+                <IonAvatar>
+                  <img
+                    alt="Silhouette of a person's head"
+                    src="https://ionicframework.com/docs/img/demos/avatar.svg"
+                  />
+                </IonAvatar>
+              </div>
+              <IonItem>
+                <IonInput
+                  label="Username"
+                  placeholder="enter username"
+                  
+
+                ></IonInput>
+              </IonItem>
+              <IonItem>
+                <IonInput
+                  type="password"
+                  label="Password"
+                  value="edsheram24"
+                >
+                  <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
+                </IonInput>
+              </IonItem>
+              <IonButton onClick={() => doLogin()} expand="full">
+                Login
+              </IonButton>
+            </IonList>
+          </IonCardContent>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
